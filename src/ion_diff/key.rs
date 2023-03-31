036779554d87b549e26a9b8a63962e5446c482c8
Matthew Pope <popematt@amazon.com>
2023-03-31 09:48:24 -0700

    Fixes diffs for structs

---
+++ src/ion_diff/key.rs
@@ -0,0 +1,46 @@
+// Copyright Amazon.com, Inc. or its affiliates.
+
+use crate::element::Value;
+use crate::{Element, Symbol};
+use std::cmp::Ordering;
+use std::fmt;
+use crate::ion_diff::ord_element::OrdElement;
+
+/// A representation of all key types typical Value types will assume.
+#[derive(Debug, Clone, PartialEq, Eq, Ord, PartialOrd)]
+pub enum Key {
+    /// An array index
+    Index(usize),
+    /// A string index for mappings
+    Symbol(Symbol),
+}
+
+impl fmt::Display for Key {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Key::Symbol(ref v) => v.fmt(f),
+            Key::Index(ref v) => v.fmt(f),
+        }
+    }
+}
+
+impl From<&Symbol> for Key {
+    fn from(value: &Symbol) -> Self {
+        Key::Symbol(value.clone())
+    }
+}
+impl From<usize> for Key {
+    fn from(value: usize) -> Self {
+        Key::Index(value)
+    }
+}
+
+impl From<&Key> for Value {
+    fn from(value: &Key) -> Self {
+        match value {
+            Key::Index(i) => Value::Int((*i).into()),
+            Key::Symbol(s) => Value::Symbol(s.into()),
+        }
+    }
+}
+
---
+++ src/ion_diff/ord_element.rs
@@ -0,0 +1,107 @@
+
+
+use std::cmp::{max, Ordering};
+use std::fmt::{Display, Formatter};
+use std::ops::Deref;
+use crate::element::{Element, IonSequence, Struct, Value};
+use crate::ion_diff::recorder::DefaultChangeListener;
+use rstest::rstest;
+use crate::{IonType, Symbol};
+
+#[derive(Copy, Clone)]
+pub(crate) struct OrdElement<'a>(&'a Element);
+
+impl <'a> Display for OrdElement<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(self.0, f)
+    }
+}
+
+impl <'a> From<&'a Element> for OrdElement<'a> {
+    fn from(value: &'a Element) -> Self {
+        OrdElement(value)
+    }
+}
+
+impl <'a> From<OrdElement<'a>> for &'a Element {
+    fn from(value: OrdElement<'a>) -> Self {
+        value.0
+    }
+}
+
+impl <'a> PartialEq for OrdElement<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+impl <'a> Eq for OrdElement<'a> {}
+
+impl <'a> PartialOrd for OrdElement<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(Ord::cmp(self, other))
+    }
+}
+
+impl <'a> Ord for OrdElement<'a> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        cmp_elements(&self.0, &other.0)
+    }
+}
+
+fn cmp_elements(this: &Element, that: &Element) -> Ordering {
+    let ord = IonType::partial_cmp(&this.ion_type(), &that.ion_type()).unwrap();
+    if !ord.is_eq() { return ord; }
+
+    let a1 = this.annotations().map(|s| s.text()).collect::<Vec<_>>();
+    let a2 = that.annotations().map(|s| s.text()).collect::<Vec<_>>();
+
+    let ord = a1.cmp(&a2);
+    if !ord.is_eq() { return ord; }
+
+    match this.value() {
+        Value::Null(_) => Ordering::Equal,
+        Value::Int(this) => this.cmp(that.as_int().unwrap()),
+        Value::Float(this) => this.total_cmp(&that.as_float().unwrap()),
+        Value::Decimal(this) => this.cmp(that.as_decimal().unwrap()),
+        Value::Timestamp(this) => this.cmp(that.as_timestamp().unwrap()),
+        Value::String(this) => this.as_str().cmp(that.as_string().unwrap()),
+        Value::Symbol(this) => this.cmp(that.as_symbol().unwrap()),
+        Value::Bool(this) => this.cmp(&that.as_bool().unwrap()),
+        Value::Blob(this) => this.as_slice().cmp(that.as_blob().unwrap()),
+        Value::Clob(this) => this.as_slice().cmp(that.as_clob().unwrap()),
+        Value::SExp(this) => cmp_seq_by(this.elements(), that.as_list().unwrap().elements(), cmp_elements),
+        Value::List(this) => cmp_seq_by(this.elements(), that.as_list().unwrap().elements(), cmp_elements),
+        Value::Struct(this) => cmp_structs(this, &that.as_struct().unwrap()),
+    }
+}
+
+fn cmp_structs(this: &Struct, that: &Struct) -> Ordering {
+    let mut these_fields = this.fields().collect::<Vec<(&Symbol, &Element)>>();
+    let mut those_fields = that.fields().collect::<Vec<(&Symbol, &Element)>>();
+    these_fields.sort_by(cmp_fields);
+    those_fields.sort_by(cmp_fields);
+    cmp_seq_by(these_fields.iter().map(|x|x), those_fields.iter().map(|x|x), cmp_fields)
+}
+
+fn cmp_fields(this: &(&Symbol, &Element), that: &(&Symbol, &Element)) -> Ordering {
+    let ord = this.0.text().cmp(&that.0.text());
+    if !ord.is_eq() { return ord; }
+    cmp_elements(this.1, that.1)
+}
+
+fn cmp_seq_by<T, R: Deref<Target = T>, I1: IntoIterator<Item = R>, I2: IntoIterator<Item = R>>(mut this: I1, mut that: I2, cmp: fn(&T, &T) -> Ordering) -> Ordering {
+    let mut that = that.into_iter();
+    for this_i in this {
+        let that_i = that.next();
+        let ord = match that_i {
+            Some(that_i) => cmp(&this_i, &that_i),
+            None => Ordering::Greater,
+        };
+        if !ord.is_eq() { return ord; }
+    }
+    if that.next().is_some() {
+        Ordering::Less
+    } else {
+        Ordering::Equal
+    }
+}
